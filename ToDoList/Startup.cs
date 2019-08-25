58e1dd962b265cd754e8a5eed0c59425b09202ae
Young Liu <youngzliu3@gmail.com>
2019-08-25 10:53:27 -0700

    add sql database connector

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -41,4 +41,10 @@ namespace ToDoList
 
     }
   }
+
+  public static class DBConfiguration
+  {
+    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
+  }
+
 }
