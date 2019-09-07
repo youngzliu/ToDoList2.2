e5220ace351ea9f5a1e31230cb73c3617f403d9e
Young Liu <youngzliu3@gmail.com>
2019-09-06 22:55:14 -0700

    add ability to retrive items from the database using entity

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,35 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/categories/{categoryId}/items/new")]
-    public ActionResult New(int categoryId)
+    private readonly ToDoListContext _db;
+
+    public ItemsController(ToDoListContext db)
     {
-      Category category = Category.Find(categoryId);
-      return View(category);
+      _db = db;
     }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
+    public ActionResult Index()
     {
-      Item.ClearAll();
-      return View();
-    }
-
-    [HttpGet("/categories/{categoryId}/items/{itemId}")]
-    public ActionResult Show(int categoryId, int itemId)
-    {
-      Item item = Item.Find(itemId);
-      Category category = Category.Find(categoryId);
-      Dictionary<string, object> model = new Dictionary<string, object>();
-      model.Add("item", item);
-      model.Add("category", category);
+      List<Item> model = _db.Items.ToList();
       return View(model);
     }
-
   }
 }
