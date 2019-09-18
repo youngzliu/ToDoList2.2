fe9c6a0876749ae8ac460854ed311ec474b2a7c4
Young Liu <youngzliu3@gmail.com>
2019-09-17 19:21:02 -0700

    update items to have relationships using EF

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
@@ -16,7 +17,7 @@ namespace ToDoList.Controllers
 
     public ActionResult Index()
     {
-      List<Item> model = _db.Items.ToList();
+      List<Item> model = _db.Items.Include(items => items.Category).ToList();
       return View(model);
     }
 
--- ToDoList/Models/ToDoListContext.cs
+++ ToDoList/Models/ToDoListContext.cs
@@ -4,6 +4,7 @@ namespace ToDoList.Models
 {
   public class ToDoListContext : DbContext
   {
+    public virtual DbSet<Category> Categories { get; set; } //new line
     public DbSet<Item> Items { get; set; }
 
     public ToDoListContext(DbContextOptions options) : base(options) { }
