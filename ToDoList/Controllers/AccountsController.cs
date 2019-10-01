f83f495a529e7a53d88dbb2d26629dd4f27d4250
Young Liu <youngzliu3@gmail.com>
2019-10-01 11:22:29 -0700

    add controller actions to display view and post method to login

--- ToDoList/Controllers/AccountsController.cs
+++ ToDoList/Controllers/AccountsController.cs
@@ -43,5 +43,24 @@ namespace ToDoList.Controllers
                 return View();
             }
         }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
