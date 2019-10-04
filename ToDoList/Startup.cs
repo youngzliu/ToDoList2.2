dcbe1f82cfc6c7912a0cafe7b7fbcd7d5fa80c70
Young Liu <youngzliu3@gmail.com>
2019-10-04 15:15:20 -0700

    change password requirements for development

--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -29,10 +29,20 @@ namespace ToDoList
         .AddDbContext<ToDoListContext>(options => options
         .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
 
-      //new code
+      
       services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ToDoListContext>()
                 .AddDefaultTokenProviders();
+
+      services.Configure<IdentityOptions>(options =>
+      {
+          options.Password.RequireDigit = false;
+          options.Password.RequiredLength = 0;
+          options.Password.RequireLowercase = false;
+          options.Password.RequireNonAlphanumeric = false;
+          options.Password.RequireUppercase = false;
+          options.Password.RequiredUniqueChars = 0;
+      });
     }
 
     public void Configure(IApplicationBuilder app)
